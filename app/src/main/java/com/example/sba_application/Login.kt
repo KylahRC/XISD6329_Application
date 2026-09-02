338c27c76eb09a9fe91b5c81e3ad2dd825754019
Kylah Irvine <ST10435066@rcconnect.edu.za>
2026-09-02 15:48:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sba_application/Login.kt
@@ -0,0 +1,107 @@
+package com.example.sba_application
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+    var editTextEmail: TextInputEditText? = null
+    var editTextPassword: TextInputEditText? = null
+    var buttonLogin: Button? = null
+    var mAuth: FirebaseAuth? = null
+    var progressBar: ProgressBar? = null
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null)
+        val currentUser = mAuth!!.getCurrentUser()
+        if (currentUser != null) {
+            val intent = Intent(getApplicationContext(), MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
+
+        // Initialise the objects
+        editTextEmail = findViewById<TextInputEditText>(R.id.email)
+        editTextPassword = findViewById<TextInputEditText>(R.id.password)
+        buttonLogin = findViewById<Button>(R.id.btn_login)
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        buttonLogin!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                progressBar!!.setVisibility(View.VISIBLE)
+                val email: String?
+                val password: String?
+                email = editTextEmail!!.getText().toString().toString()
+                password = editTextPassword!!.getText().toString().toString()
+
+                if (TextUtils.isEmpty(email)) {
+                    val context: Context?
+                    val text: String?
+                    Toast.makeText(
+                        this@Login.also { context = it },
+                        "Enter email".also { text = it },
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return
+                }
+                if (TextUtils.isEmpty(password)) {
+                    val context: Context?
+                    val text: String?
+                    Toast.makeText(
+                        this@Login.also { context = it },
+                        "Enter Password".also { text = it },
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return
+                }
+
+                mAuth!!.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(object : OnCompleteListener<AuthResult?> {
+                        override fun onComplete(task: Task<AuthResult?>) {
+                            progressBar!!.setVisibility(View.GONE)
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                val text: String?
+                                Toast.makeText(
+                                    getApplicationContext(),
+                                    "Login Successful".also { text = it },
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                val intent =
+                                    Intent(getApplicationContext(), MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                // If sign in fails, display a message to the user.
+
+                                Toast.makeText(
+                                    this@Login, "Authentication failed.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    })
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/sba_application/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.sba_application
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class MainActivity : ComponentActivity() {
+    var auth: FirebaseAuth? = null
+    var button: Button? = null
+    var textView: TextView? = null
+    var user: FirebaseUser? = null
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        button = findViewById<Button>(R.id.logout)
+        textView = findViewById<TextView>(R.id.user_details)
+        user = auth!!.getCurrentUser()
+
+        if (user == null) {
+            val intent = Intent(getApplicationContext(), Login::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            textView!!.setText(user!!.getEmail())
+        }
+        button!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(getApplicationContext(), Login::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
+    }
+}
