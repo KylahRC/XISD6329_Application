d8aabd881a1193f88b5bf93f0cdb3bfa7e4d18bd
Kylah Irvine <ST10435066@rcconnect.edu.za>
2026-09-03 12:09:39 +0200

    Created data class for firebase data

---
+++ app/src/main/java/com/example/sba_application/HostData.kt
@@ -0,0 +1,12 @@
+package com.example.sba_application
+
+data class HostData
+    (
+        val Host_Name: String = "",
+        val Host_Surname: String = "",
+        val Host_Address: String = "",
+        val Host_Area: String = "",
+        val Host_Bathroom: String = "",
+        val Host_PhoneNumber: String = ""
+    )
+//Made the same way as last semesters projects, and how we learnt in OPSC
